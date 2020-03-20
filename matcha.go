4a12f9291222d15e2008315f9e19cf7d55b1dd09
Connor Zanin <cnnrznn@gmail.com>
2020-03-20 12:23:50 -0400

    A working minimal app

--- matcha.go
+++ matcha.go
@@ -1,44 +1,59 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"sync"
+
+	_ "github.com/lib/pq"
 )
 
 type server struct {
 	mux sync.Mutex
+	pool *sql.DB
+}
+
+func newServer() *server {
+	s := &server{}
+	pool, err := sql.Open("postgres",
+		"user=cnnrznn password=matchatime dbname=matcha")
+	if err != nil {
+		log.Fatal("Could not open database connection")
+	}
+	s.pool = pool
+
+	return s
 }
 
 func main() {
-	s := &server{}
+	s := newServer()
 
 	mux := http.NewServeMux()
 	mux.Handle("/", s)
 	mux.Handle("/inc", s)
-	mux.Handle("/dec", s)
 	mux.Handle("/get", s)
 
 	log.Fatal(http.ListenAndServe(":3030", mux))
 }
 
 func (s *server) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	w.WriteHeader(http.StatusOK)
 	switch req.URL.Path {
 	case "/":
+		log.Println("Hit: /")
 		if !s.serveHome(w) {
 			w.WriteHeader(http.StatusInternalServerError)
 		}
 	case "/inc":
-		if !s.increment() {
-			w.WriteHeader(http.StatusInternalServerError)
-		}
-	case "/dec":
-		if !s.decrement() {
+		log.Println("Hit: /inc")
+		if !s.increment(req) {
 			w.WriteHeader(http.StatusInternalServerError)
 		}
 	case "/get":
+		log.Println("Hit: /get")
 		val := s.get()
 		w.Write([]byte(fmt.Sprintf(`{"val": %v}`, val)))
 	default:
@@ -56,20 +71,29 @@ func (s *server) serveHome(w http.ResponseWriter) bool {
 	return true
 }
 
-func (s *server) increment() bool {
+func (s *server) increment(req *http.Request) bool {
 	s.mux.Lock()
 	defer s.mux.Unlock()
 
-	return false
-}
+	src := req.URL.Query()["src"]
+	query := fmt.Sprintf("INSERT INTO log VALUES (now(), '%v');", src)
+	_, err := s.pool.Query(query)
 
-func (s *server) decrement() bool {
-	s.mux.Lock()
-	defer s.mux.Unlock()
+	if err != nil {
+		log.Println(err)
+		return false
+	}
 
-	return false
+	return true
 }
 
 func (s *server) get() int {
-	return 0
+	result := 0
+	rows, _ := s.pool.Query("SELECT COUNT(*) FROM log")
+	defer rows.Close()
+
+	rows.Next()
+	rows.Scan(&result)
+
+	return result
 }
